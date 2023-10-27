e62115c8af5d82df3f639c74f0e985c43129a0d9
Aron <hansick001@gmail.com>
2023-10-27 10:12:01 +0900

    초기값 설정 추가

--- project17_03/webapp/controller/Main.controller.js
+++ project17_03/webapp/controller/Main.controller.js
@@ -9,11 +9,14 @@ sap.ui.define([
 
         return Controller.extend("project1703.controller.Main", {
             onInit: function () {
-
+                // 초기값 세팅 - 화면이 뜨자마자 각 Input에 10, 20 세팅
+                this.byId('idInput1').setValue("10");
+                this.byId('idInput2').setValue("20");
             },
 
             calCulator: function() {
                 let item1 = this.getView().byId("idInput1").getValue()*1;
+                // let item1 = this.byId("idInput1").getValue()*1;     getView() 생략가능
                 let item2 = this.getView().byId("idInput2").getValue()*1;
                 let op = this.getView().byId("idSelect").getSelectedKey();
                 let result = 0;
@@ -35,6 +38,7 @@ sap.ui.define([
                 }
                 // console.log( result );
                 sap.m.MessageToast.show(result);
+                // sap.m.MessageBox
             }
         });
     });
