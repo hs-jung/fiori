7a606abb98c2332319e6c73f8b76976f1a8a78f0
Aron <hansick001@gmail.com>
2023-10-31 16:48:16 +0900

    Modal Dialog 생성/추가

--- project17_06/webapp/controller/HelloPanel.controller.js
+++ project17_06/webapp/controller/HelloPanel.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment) {
         "use strict";
 
         return Controller.extend("project1706.controller.HelloPanel", {
             onInit: function () {
 
+            },
+            onOpenDialog: function(oEvent) {
+                // sap.ui.core.Fragment
+                Fragment.load({
+                    name : 'project1706.view.fragment.Dialog',
+                    type : 'XML'
+                }).then(function(oDialog) {
+                    //비동기로 file load 끝난 후 then 함수 시작
+                    oDialog.open();
+                });
             }
-        });s
+        });
     });
