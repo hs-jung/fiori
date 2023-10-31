0e3c751dac5d5b38ec949918bf5326007989de12
Aron <hansick001@gmail.com>
2023-10-31 15:43:52 +0900

    Create New Project06

---
+++ project17_06/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"project17_06/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
---
+++ project17_06/webapp/test/unit/controller/main.controller.js
@@ -0,0 +1,16 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"project17_06/controller/main.controller"
+], function (Controller) {
+	"use strict";
+
+	QUnit.module("main Controller");
+
+	QUnit.test("I should test the main controller", function (assert) {
+		var oAppController = new Controller();
+		oAppController.onInit();
+		assert.ok(oAppController);
+	});
+
+});
