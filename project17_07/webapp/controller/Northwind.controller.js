5fe868378809e3c7d91ec516ffd4557511011d90
Aron <hansick001@gmail.com>
2023-11-02 13:13:17 +0900

    path / formatter 추가

--- project17_07/webapp/controller/Northwind.controller.js
+++ project17_07/webapp/controller/Northwind.controller.js
@@ -10,6 +10,29 @@ sap.ui.define([
         return Controller.extend("odata.project1.controller.Northwind", {
             onInit: function () {
 
+            },
+            //formatter 함수
+            formatter : {
+                fnDateToString: function(value) {
+                    if(value){
+                        // var date = new Date(value);
+                        // result = date.getFullYear().toString() + "-" + date.getMonth().toString() + "-"  + date.getDate().toString() + " " + date.getHours().toString() + ":" + date.getMinutes().toString() + ":" + date.getSeconds().toString();
+    
+                        var oFormat = sap.ui.core.format.DateFormat.getDateInstance({
+                            pattern : 'yyyy-MM-dd HH:mm:ss'
+                        });
+                        return oFormat.format(value);
+    
+                    }
+                    
+                },
+                fnFreightSum : function(via, freight) {
+                    if( via && freight){
+                        return via * Number(freight);
+                    }
+                }
+                
             }
+            
         });
     });
