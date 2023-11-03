d97296164eafd94b58a6f0856084ef4c7708c734
Aron <hansick001@gmail.com>
2023-11-03 11:46:15 +0900

    기초다지기 문제5 - history 기능 추가

--- ux400_solving/webapp/controller/Main.controller.js
+++ ux400_solving/webapp/controller/Main.controller.js
@@ -1,18 +1,31 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"  
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,JSONModel) {
         "use strict";
 
         return Controller.extend("sap.btp.ux400solving.controller.Main", {
             onInit: function () {
+                //Data 설정
+                var oData = { rows : [] };         //json data
+
+                var oModel = new JSONModel(oData); 
+                this.getView().setModel(oModel,"list");
 
             },
             onRandomPress : function () {
-                this.byId("idInput").setValue(Math.floor(Math.random() * 100) + 1);
+                var value = Math.floor(Math.random() * 100) + 1;
+                this.byId("idInput").setValue(value);
+
+                let oModel = this.getView().getModel('list');
+                let oData = this.getView().getModel('list').getProperty("/rows");
+
+                oData.unshift({text : value});  //DES
+                oModel.setProperty("/rows", oData);
             }
         });
     });
