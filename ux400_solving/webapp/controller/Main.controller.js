bb22ee9991c873a9d2ca7bf557545c8471ee1fad
Aron <hansick001@gmail.com>
2023-11-03 12:31:24 +0900

    기초다지기 문제6  - Fragment

--- ux400_solving/webapp/controller/Main.controller.js
+++ ux400_solving/webapp/controller/Main.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"  
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel) {
+    function (Controller,JSONModel,Fragment) {
         "use strict";
 
         return Controller.extend("sap.btp.ux400solving.controller.Main", {
@@ -26,6 +27,33 @@ sap.ui.define([
 
                 oData.unshift({text : value});  //DES
                 oModel.setProperty("/rows", oData);
+            },
+            onOpenPress : function() {
+                // debugger;
+                var oDialog =  sap.ui.getCore().byId("idDialog");
+                var oModel = this.getView().getModel();
+                // sap.ui.core.Fragment
+                if(!oDialog){
+                    Fragment.load({
+                        name : 'sap.btp.ux400solving.view.fragment.Products',
+                        type : 'XML',
+                        controller : this
+                    }).then(function(oDialog) {
+                        //비동기로 file load 끝난 후 then 함수 시작
+                        oDialog.setModel(oModel);
+                        oDialog.open();
+                    });
+                }
+                else{
+                    oDialog.open();
+                }
+            },
+            onClose : function(oEvent) {
+                var oDialog =  sap.ui.getCore().byId("idDialog");
+                // oDialog.close();
+
+                //이벤트를 일으킨 객체(버튼)로부터 접근하여 Dialog 닫기
+                oEvent.getSource().getParent().close();
             }
         });
     });
