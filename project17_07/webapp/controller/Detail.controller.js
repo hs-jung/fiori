040820ca5a434f9fe0739345f4458ba22c92073a
Aron <hansick001@gmail.com>
2023-11-06 14:37:27 +0900

    Detail view/controller 추가

---
+++ project17_07/webapp/controller/Detail.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller,Fragment, Filter,FilterOperator) {
+        "use strict";
+
+        var _oModel; //클로저변수
+
+        return Controller.extend("odata.project1.controller.Detail", {
+            onInit: function () {
+               
+            }
+        });
+    });
