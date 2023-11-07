03ca7aed9b8cca1566f4737fc85f700b170c0720
Aron <hansick001@gmail.com>
2023-11-07 13:05:56 +0900

    line, pie Chart 추가

--- project17_08/webapp/controller/Main.controller.js
+++ project17_08/webapp/controller/Main.controller.js
@@ -1,15 +1,27 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("project1708.controller.Main", {
             onInit: function () {
+                var oData = {
+                    list : [
+                        { name : 'aaa', rate : 35, cost : 10 },
+                        { name : 'bbb', rate : 15, cost : 12 },
+                        { name : 'ccc', rate : 10, cost : 11 },
+                        { name : 'ddd', rate : 15, cost : 15 },
+                        { name : 'eee', rate : 20, cost : 10 },
+                        { name : 'fff', rate : 5, cost : 16 }
+                    ]
+                };
 
+                this.getView().setModel(new JSONModel(oData), 'view');
             }
         });
     });
