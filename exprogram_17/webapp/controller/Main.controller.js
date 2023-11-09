3471437318151e693825b34281a8305d31f06478
Aron <hansick001@gmail.com>
2023-11-09 12:07:53 +0900

    문제2 테이블 필터 및 검색 기능 추가

--- exprogram_17/webapp/controller/Main.controller.js
+++ exprogram_17/webapp/controller/Main.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("exam.exprogram17.controller.Main", {
@@ -12,7 +14,26 @@ sap.ui.define([
 
             },
             onSearch : function() {
-                debugger;
+                var categoryID = ""
+                var categoryName = ""
+                var oFilter = new Filter();
+                var aFilter = [];
+
+                if (this.byId("idCategoryID").getValue()) {
+                    categoryID = this.byId("idCategoryID").getValue();
+                    var oFilter = new Filter('CategoryID','GE', categoryID);
+                    aFilter.push(oFilter);
+                }
+
+                if (this.byId("idCategoryName").getValue()) {
+                    categoryName = this.byId("idCategoryName").getValue();
+                    var oFilter = new Filter('CategoryName','Contains', categoryName);
+                    aFilter.push(oFilter);
+                }
+
+                //테이블 객체 가져와서, 바인딩 정보 가져온 후, Filter 적용
+                this.byId("idTable").getBinding("items").filter(aFilter);
+                
             }
         });
     });
